c518f1d4ecd98bb9de9037acde39ae448e8af6c9
go2375 <malgorzata.ryczer-dumas@isen-ouest.yncrea.fr>
2025-10-23 16:39:42 +0200

    Models.py : connexion à la base de données et ensemble d'architecture models.py

---
+++ app/models.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+## Permet de préciser les types : Optional et Dict, où les clés sont toujours des chaînes de caractères
+# (par exemple "username", "role_id", etc.) et les valeurs peuvent être de n’importe quel type : int, float, str, bool ou None.
+from typing import Optional, List, Dict, Any
+
+# On import objet Config de config.py
+from app.config import Config
+
+# Pour gérer la connexion à la base de données
+class Database:
+    # @staticmethod dans une classe n’a pas besoin d’une instance pour être appelé.
+    @staticmethod
+    def get_connection():
+        # Ouvrir la base SQLite
+        conn = sqlite3.connect(Config.DATABASE_PATH)
+        # Permet de récupérer les lignes comme dictionnaire
+        conn.row_factory = sqlite3.Row
+        # Active les clés étrangères, désactivées par défaut par SQLite.
+        conn.execute("PRAGMA foreign_keys = ON")
+        # Permet de renvoyer l’objet conn, l’objet de connexion SQLite,
+        # afin que les autres parties du code puissent utiliser cette connexion pour exécuter
+        # des requêtes SQL (SELECT, INSERT, UPDATE, etc.).
+        return conn
+    
+    @staticmethod
+    # sqlite3.Row est un format interne de SQLite qui permet de convertir une ligne en dictionnaire Python, 
+    # où les clés du dictionnaire sont les noms des colonnes (username, role_id, etc.)
+    # et les valeurs sont les données correspondantes dans cette ligne.
+    # Ce dictionnaire Python sera automatiquement converti en JSON lorsque l’API enverra
+    # une réponse à une requête soumise par un client.
+    def dict_from_row(row: sqlite3.Row) -> Dict[str, Any]:
+        # row[key] correspond à la valeur d’une colonne pour la ligne actuelle.
+        # for key in row.keys() parcourit la liste des noms de colonnes.
+        # {key: row[key] ...} crée un dictionnaire où chaque clé correspond au nom d’une colonne
+        # et chaque valeur à la donnée correspondante.
+        # Ainsi, dict_from_row(row) convertit une ligne SQL en dictionnaire Python,
+        # prêt à être utilisé par l’API.
+        return {key: row[key] for key in row.keys()}
+
+# # Modèle pour la table Role
+# class RoleModel:
+
+# # Modèle pour la table Utilisateur
+# class UtilisateurModel:
+
+# # Modèle pour la table Site_Touristique
+# class SiteModel:
+
+# # Modèle pour la table Parcours
+# class ParcoursModel:
+
+# # Modèle pour la table Commune
+# class CommuneModel:
+
+# # Modèle pour la table Department
+# class DepartmentModel:
